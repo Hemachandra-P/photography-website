7052d8eff3c1261c6f4b1b6906ab812ad05b77d4
Hemachandra-P <hemachandrajoy176@gmail.com>
2026-06-16 13:05:08 +0530

    Render deployment setup

--- photographyjoy/settings.py
+++ photographyjoy/settings.py
@@ -129,5 +129,11 @@ MEDIA_ROOT = BASE_DIR / 'media'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    ...
+
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
